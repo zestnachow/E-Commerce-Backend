bd4c78655075cef8d37005733cb9da0f4fa00c8b
Zach Weston <zachpweston@gmail.com>
2023-01-29 22:30:13 -0700

    all models complete

--- models/ProductTag.js
+++ models/ProductTag.js
@@ -6,7 +6,28 @@ class ProductTag extends Model {}
 
 ProductTag.init(
   {
-    // define columns
+    id: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      primaryKey: true,
+      autoIncrement: true,
+    },
+    product_id: {
+      type: DataTypes.INTEGER,
+      references: {
+        model: "Product",
+        key: "id",
+        unique: false,
+      },
+    },
+    tag_ig: {
+      type: DataTypes.INTEGER,
+      references: {
+        model: "Tag",
+        key: "id",
+        unique: false,
+      },
+    },
   },
   {
     sequelize,
